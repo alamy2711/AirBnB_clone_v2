28ac5a10adcf2f0d2f1b6310c92aca062cc86939
Alamy <mestafaalami11@gmail.com>
2024-08-23 01:43:24 +0100

    Update 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,10 +10,11 @@ app = Flask(__name__)
 def hello_world():
     """ Returns text """
     return 'Hello HBNB!'
+
+
 @app.route('/hbnb', strict_slashes=False)
 def hello():
     """ Return text """
     return 'HBNB'
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-    
